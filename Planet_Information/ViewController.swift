16943f4e93b459f539dece62ef3170dddc83ff23
Chawan Saeed <chawan.saeed02@gmail.com>
2020-12-05 01:21:07 +0300

    Initial Commit

---
+++ Planet_Information/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Planet_Information
+//
+//  Created by Chawan Saeed on 12/5/20.
+//  Copyright © 2020 Chawan Saeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
